52a15f2d55e920ebf4c0d4af973c1408cf46c70e
Rafael Magalhães <superrfm@hotmail.com>
2019-04-21 21:24:16 -0300

    Add project files.

---
+++ rfmagalhaes.aspnetcorewebapi/Controllers/PedidoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rfmagalhaes.aspnetcorewebapi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class PedidoController
+        : ControllerBase
+    {
+        // /Pedido - GET
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(int), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult PedidoPorId([FromRoute] int id) {
+
+            if (id <= 0) return BadRequest(id);
+            return Ok(id);
+        }
+    }
+}
---
+++ rfmagalhaes.aspnetcorewebapi/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace rfmagalhaes.aspnetcorewebapi
+{
+    public class Startup
+    {
+        private readonly IHostingEnvironment _env;
+        private readonly IConfiguration _config;
+        private readonly ILoggerFactory _loggerFactory;
+
+        public Startup(IHostingEnvironment env, IConfiguration config, ILoggerFactory loggerFactory)
+        {
+            _env = env ?? throw new ArgumentNullException($"IHostingEnvironment não foi injetado");
+            _config = config ?? throw new  ArgumentNullException($"IConfiguration não foi injetado");
+            _loggerFactory = loggerFactory ?? throw new ArgumentNullException($"ILoggerFactory não foi injetado.");
+        }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddMvcCore()
+                .AddJsonFormatters()
+                .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_2);
+
+            var logger = _loggerFactory.CreateLogger<Startup>();
+
+            if (_env.IsDevelopment()) {
+                logger.LogInformation("Aplicação em modo Development");
+            }
+
+          
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            if (_env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
